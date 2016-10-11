c5980354200a35d7350ad18f99237bab6d2cc690
Tom Sitter <thomas.sitter@gmail.com>
2016-10-11 15:24:22 -0400

    Add reidentification, change encrypted dict format

--- deidentifier.py
+++ deidentifier.py
@@ -6,6 +6,7 @@ import string
 from simplecrypt import encrypt, decrypt
 from getpass import getpass
 import pickle
+import sys
 
 def deidentify_multiple(emr='accuro', target_fields=None, filenames=None, outputs=None):
     """ Calls deidentify with multiple input and output files. This allows deidentification
@@ -38,6 +39,9 @@ def deidentify(emr='accuro', target_fields=None, filename=None, output=None, uni
                    output='tests/unidentifiable.csv')
 
         Note: If deidentifying multiple files, the target fields must be the same in all files
+
+        Certain relationships can be maintained (e.g. patient to doctor) by doing two deidentifications
+        Each time, specify a different column to be deidentified.
     """
     assert os.path.exists(filename)
 
@@ -48,47 +52,46 @@ def deidentify(emr='accuro', target_fields=None, filename=None, output=None, uni
 
         # Make sure target_fields is a list
         if not isinstance(target_fields, list):
-            target_fields = list(target_fields)
+            target_fields = [target_fields]
 
         # Make sure all fields to deidentify are in the file
         assert set(target_fields).issubset(fieldnames)
 
         # Open a CSV file to write to, write header
-        out = open(output, 'w')
-        writer = csv.writer(out, lineterminator='\n')
-        writer.writerow(fieldnames)
+        with open(output, 'w') as out:
+            writer = csv.writer(out, lineterminator='\n')
+            writer.writerow(fieldnames)
 
-        # When deidentifying multiple files we may be passed a dict of currently
-        # deidentified entries that have already been compiled, otherwise initialize one
-        if unique_entries is None:
-            unique_entries = {}
+            # When deidentifying multiple files we may be passed a dict of currently
+            # deidentified entries that have already been compiled, otherwise initialize one
+            if unique_entries is None:
+                unique_entries = {
+                    'entries': {},
+                    'fields': target_fields
+                }
+            # Loop through the file and write to output in a single pass
+            for row in reader:
+                new_row = []
 
-        # Loop through the file and write to output in a single pass
-        for row in reader:
-            new_row = []
+                # get the identifiable columns from the row that need to be deidentified
+                entry = tuple(row[field] for field in target_fields)
 
-            # get the identifiable columns from the row that need to be deidentified
-            entry = tuple(row[field] for field in target_fields)
-
-            # if we have already deidentified this row, let's replace those columns
-            if entry in unique_entries:
-                deidentified_entry = unique_entries[entry]
-            else:
-                deidentified_entry = deidentify_row(row, target_fields)
-                unique_entries[entry] = deidentified_entry
-
-            # fieldnames preserves ordering of fields in the original CSV file
-            for field in fieldnames:
-                if field in deidentified_entry.keys():
-                    new_row.append(deidentified_entry[field])
+                # if we have already deidentified this row, let's replace those columns
+                if entry in unique_entries['entries']:
+                    deidentified_entry = unique_entries['entries'][entry]
                 else:
-                    new_row.append(row[field])
+                    deidentified_entry = deidentify_row(row, target_fields)
+                    unique_entries['entries'][entry] = deidentified_entry
 
-            writer.writerow(new_row)
+                # fieldnames preserves ordering of fields in the original CSV file
+                temp = dict(zip(target_fields, deidentified_entry))
+                for field in fieldnames:
+                    if field in target_fields:
+                        new_row.append(temp[field])
+                    else:
+                        new_row.append(row[field])
 
-        # close the output file
-        reader.close()
-        out.close()
+                writer.writerow(new_row)
 
         print('Finished!')
 
@@ -96,19 +99,58 @@ def deidentify(emr='accuro', target_fields=None, filename=None, output=None, uni
             save_encrypted_dict(unique_entries)
 
 
+def reidentify(filename=None, output=None):
+
+    assert os.path.exists(filename)
+
+    with open(filename) as f:
+        deid_data = load_encrypted_dict()
+        target_fields = deid_data['fields']
+        reid_rows = {v: k for k,v in deid_data['entries'].items()}
+
+        # Open the encrypted file and make sure the fields in each match
+        reader = EMRDictReader(f, emr='accuro')
+        if not all(field in reader.fieldnames for field in target_fields):
+            sys.exit("Mismatch between fields in encrypted file and fields in deidentified data!")
+
+        with open(output, 'w') as out:
+            writer = csv.writer(out, lineterminator='\n')
+            writer.writerow(reader.fieldnames)
+            for row in reader:
+                patient = tuple(row[field] for field in target_fields)
+                print(patient)
+                try:
+                    identifiable_info = reid_rows[patient]
+
+                    for key, value in zip(target_fields, identifiable_info):
+                        row[key] = value
+
+                    writer.writerow([row[key] for key in reader.fieldnames])
+
+                except KeyError as err:
+                    print("Patient not found in encrypted file!")
+                    raise err
+
+    print('Finished reidentification!')
+
+
 def detect_type(entry):
     """Determines where a field is numeric, alphanumeric, or alphabetical letters only"""
+    import re
+
     if entry.isdigit():
         return 'numeric'
     elif entry.isalpha():
         return 'alpha'
     elif entry.isalnum():
         return 'alnum'
+    elif re.match(r'\d{1,2}/\d{1,2}/\d{4}', entry):
+        return 'date'
     else:
         return 'alpha'
 
 
-def replace_entry(value):
+def transform(value):
     """Returns a random string of the given type and of the same length as the original value"""
     type_ = detect_type(value)
 
@@ -116,19 +158,18 @@ def replace_entry(value):
         chars = string.digits
     elif type_ == 'alpha':
         chars = string.ascii_lowercase
+    elif type_ == 'date':
+        return dob_to_yob(value)
     else:
         chars = string.digits + string.ascii_lowercase
 
     return ''.join(random.choice(chars) for _ in range(len(value)))
 
 
-def deidentify_row(row, fields):
+def deidentify_row(row, fields, method='random'):
     """Deidentifies the specified fields in all rows of a CSV file
         Returns  dict mapping field name -> new deidentified value"""
-    deidentified_entry = {}
-
-    for field in fields:
-        deidentified_entry[field] = replace_entry(row[field])
+    deidentified_entry = tuple(transform(row[field]) for field in fields)
 
     return deidentified_entry
 
@@ -196,3 +237,20 @@ def ask_load_filename():
             return
         
     return filename
+
+
+def dob_to_yob(dob):
+    import re
+    try:
+        yob = re.search(r'\d{1,2}/\d{1,2}/(\d{4})', dob).group(1)
+    except AttributeError as err:
+        print(err)
+        assert False
+
+    return yob
+
+
+if __name__ == '__main__':
+    filename = 'C:\\Users\\admin\\Documents\\GitHub\\D2D\\tests\\data\\pss.test.deid.toreid.csv'
+    output = 'C:\\Users\\admin\\Documents\\GitHub\\D2D\\tests\\data\\pss.test.reid.csv'
+    reidentify(filename=filename, output=output)
